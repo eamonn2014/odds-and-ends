4fb762d7a9159c9d02beaf58d212ec775af5f4c6
eamonn2014 <eamonnjobrien@gmail.com>
2022-04-15 21:24:42 +0100

    cosmetic

--- Draw a Poisson PMF.R
+++ Draw a Poisson PMF.R
@@ -3,16 +3,16 @@
 
 
 rm(list=ls())
-mu=10
+mu <- 10
 xmax <-25
 ymax <- 0.15
 xx <- c(seq(0,xmax,1))
 
-yb = dpois(xx, mu)
+yb <- dpois(xx, mu)
 
 par(mar=c(2.1,4.1,4.1,4.1))
 
-b <- barplot(yb, xlim=c(0,xmax+2), ylab = "Probability", xlab="x", col='black', ylim=c(0,ymax),
+b <- barplot(yb, xlim=c(0,xmax+2), ylab = "Probability", xlab="x", col='maroon', ylim=c(0,ymax),
              main = paste0("Poisson \u03BC = ", mu, sep=""),
               names.arg= as.character(xx))
 
