7b378acc7bfb1dcfed81a4199ae67ffc21d49282
eamonn2014 <eamonnjobrien@gmail.com>
2022-04-23 06:56:56 +0100

    cosmetic update

--- faiures until success simulation.R
+++ faiures until success simulation.R
@@ -52,21 +52,21 @@ formatz2 <- function(x){
   sprintf(x, fmt = '%#.3f')  
 }
 
-foo <- function(p = 0.5, s = 4) { #p prob of success; s 
+foo <- function(p = 0.5, s = 4) { # p prob of success; s 
   
-  i <- 0
-  x <- 0 
+  i <- 0 # count of how many attempt we need to get s successes
+  x <- 0 # use this to know when to stop function
   
   while ( x < s) {  # stop when x equals s 
     
-    i <- i + 1  # count each trial
+    i <- i + 1  # count every attempt
     
     res <-  rbinom(1, size=1, prob=p)  # ala coin flip, heads is success
     
-    if (res ==1) {x=x+1}   # if heads count 
+    if (res ==1) {x=x+1}   # if heads count , if not we fail
    
   }
-  return(i) 
+  return(i) # result captured here
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ plot
@@ -77,19 +77,41 @@ foo <- function(p = 0.5, s = 4) { #p prob of success; s
   sim <- 10000
   P <- runif(1,0.05,1)   # randomly pick a probability
   s <- sample(2:10,1)    # randomly pick nth success
-  
   # simulation bar plot
   number_of_attempts <- replicate(sim, foo(p=P,s=s))  # run function many times
+  
+  Tm <- s*(1-P)/P + s
+  Sm <- mean(number_of_attempts) 
+  
+ 
   x <- table(factor(number_of_attempts, levels = s: max(number_of_attempts))) # pad out so levels so no counts included
-  b <- barplot(x/sim, main = paste0("Number of attempts until we reach ",s," successes, p=",formatz2(P),""), 
-               col="palegreen", ylab="Probability", xlab="Attempt (true negative binomial dist. blue line)")
+  b <- barplot(x/sim, main = paste0("Number of attempts until we reach ",s," successes when p=",formatz2(P),
+                                    "\n True mean ",formatz2(Tm) ," ; Simulated mean ",formatz2(Sm) ,""
+                                    
+                                    ),
+                                    
+               col="palegreen", ylab="Probability of success", xlab="Attempt (true negative binomial dist. blue line)")
   
   # overlay true neg binomial dist.
   m <- length(b)-1
   d <- dnbinom(0:m, prob=P, size=s)# *sim
-  lines(x = b, y = d , col='blue')
-  points(x = b, y = d)
+  lines(x = b, y = d , col='red')
+  points(x = b, y = d, col='red')
   # bar plot(d, names.arg= as.character(names(x)))
  
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+  
+   s*(1-P)/P + s
+   1/P*s #!
+   
+   mean(number_of_attempts)
+   my_dat <- rnbinom(n = 10000, size = s, prob = P)
+   mean(my_dat) +s
+  
+  
+  
+  
+  
+  
  
