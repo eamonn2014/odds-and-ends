8103d2ddb3ba45e5854ce8bfbe3c9a6d6520423c
eamonn2014 <eamonnjobrien@gmail.com>
2022-05-30 08:17:11 +0100

    binomial and factorials code

--- recruitment binomial probabilitites.R
+++ recruitment binomial probabilitites.R
@@ -1,7 +1,7 @@
 
-n<-29
-p<-0.45
-k<-8
+n<-7#29
+p<-.5#0.45
+k<-3#8
 
 ###after n what can we expect with success prob of p
 
@@ -19,25 +19,66 @@ library(binom)
 binom.confint(k, n, conf.level = 0.95, methods = "wilson")
  
 
-##make a factorial function.
-##probability of getting exactly k in n 
-factorial <- function (x) gamma(1 + x) 
+# equivalences
+factorial(n)
+exp(lfactorial(n))
+gamma(n+1)
+exp(lgamma(n+1))
 
-n<-n
-k<-9
+# relationships
+log(choose(n,k))
+lgamma(n+1)-lgamma(n-k+1)-lgamma(k+1)
+lgamma(n+1)-(lgamma(n-k+1)+lgamma(k+1))
+
+
+ 
+choose(n,k)*(p^k)*(1-p)^(n-k)
+
 f<-factorial(n)/((factorial(n-k)*factorial(k)))
-pi<-p
-p<-(pi^k)*(1-pi)^(n-k)
-f*p
+f*(p^k)*(1-p)^(n-k)
 
-##another way
-choose(n,k)*(pi^k)*(1-pi)^(n-k)
+f<-lfactorial(n)-((lfactorial(n-k)+lfactorial(k)))
+exp(f)*(p^k)*(1-p)^(n-k)
 
-
-
+f<-lgamma(n+1)-((lgamma(n-k+1)+lgamma(k+1)))
+exp(f)*(p^k)*(1-p)^(n-k)
 
 
 
+
+# ##make a factorial function.
+# ##probability of getting exactly k in n 
+# factorialz <- function (x) gamma(1 + x) 
+# 
+# n<-n
+# k<-9
+# f<-factorialz(n)/((factorialz(n-k)*factorialz(k)))
+# pi<-p
+# p<-(pi^k)*(1-pi)^(n-k)
+# f*p
+
+ 
+# 
+# 
+# 
+# 
+# 
+# f<- lgamma(n+1) - lgamma(n-k+1) + lgamma(k+1)
+# f <- lfactorial(n) - lfactorial(n-k) + lfactorial(k)
+# 
+# pi<-0.45
+# p<-(pi^k)*(1-pi)^(n-k)
+# exp(f)*log(p)
+#  
+# 
+# ##another way
+# choose(n,k)*(pi^k)*(1-pi)^(n-k)
+# 
+# 
+# 
+# 
+# choose(n,k)
+# 
 
 
 
